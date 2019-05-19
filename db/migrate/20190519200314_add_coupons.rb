cfc90cfa50f149589cc968d8356f67e2b0de2827
Eric Storey <estorey@gmail.com>
2019-05-19 20:05:10 +0000

    Automatically backed up by Learn

--- db/migrate/20190519200314_add_coupons.rb
+++ db/migrate/20190519200314_add_coupons.rb
@@ -1,7 +1,7 @@
 class AddCoupons < ActiveRecord::Migration[5.0]
   def change
     reate_table :Coupons do |t|
-      t.string :name
-      t.text :description
+      t.string :coupon_code
+      t.string :store
   end
 end
